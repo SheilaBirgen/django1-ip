60b210e0b4fcd943b1e4a9a108556aeedfb9bd34
SheilaBirgen <jeronobergen@gmail.com>
2020-02-28 12:56:20 +0300

    Add category to view.html

--- personal/views.py
+++ personal/views.py
@@ -4,4 +4,16 @@ from django.Http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return render(request, "landing/index.html")
+  images = Image.objects.all()
+  return render(request, "landing/index.html", {"image":images})
+
+def category(request, id):
+    '''
+    method to search images category
+    '''
+    images = Image.Object.filter(category_id=id)
+    context = {
+        "categories":categories
+        "images":images
+    }
+    return render(request, 'landing/search_result.html',context)
