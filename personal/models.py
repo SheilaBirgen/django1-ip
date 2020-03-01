e784778afa027235ca17d3ffa8f35d1ce9c55365
SheilaBirgen <jeronobergen@gmail.com>
2020-03-01 04:43:20 +0300

    perfomed the first migration

--- personal/models.py
+++ personal/models.py
@@ -2,18 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-        image = models.ImageField()
-        title = models.CharField(max_length=20, blank=True)
-        description = models.TextField()
-        location = models.ForeignKey("Location", on_delete=models.CASCADE)
-        categories = models.ForeignKey("Categories", on_delete=models.CASCADE)
-
-
+    image = models.ImageField()
+    title = models.CharField(max_length=20, blank=True)
+    description = models.TextField()
+    location = models.ForeignKey("Location", on_delete=models.CASCADE)
+    categories = models.ForeignKey("Categories", on_delete=models.CASCADE)
 
     @classmethod
     def search_by_category(cls, search_term):
         '''
-        Method to filter images by category
+             Method to filter images by category
         '''
         result = cls.objects.filter(categories__icontains=search_term)
         return result
