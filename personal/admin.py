96c69e2b1af900d5bccbd4d41d25db19fb1d1cbb
SheilaBirgen <jeronobergen@gmail.com>
2020-03-01 20:56:06 +0300

    add search method to views.py

--- personal/admin.py
+++ personal/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Image,Location,Category
+from .models import Image,Location,Categories
 
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Location)
-admin.site.register(Category)
+admin.site.register(Categories)
 
 
--- personal/views.py
+++ personal/views.py
@@ -7,6 +7,20 @@ def index(request):
 #   images = Image.objects.all()
   return render(request, "landing/index.html")
 
+def search_result(request):
+    '''
+    Method to search location or category
+    '''
+    if 'result' in request.GET and request.GET["result"]:
+        search_term = request.GET.get("result")
+        searched_images = Image.search_by_category(search_term)
+       
+        message = f"{search_term}"
+        return render(request, 'search.html', {"message":message, "images":searched_images})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search_results.html', {"message":message})
+      
 def category(request, id):
     '''
     method to search images category
