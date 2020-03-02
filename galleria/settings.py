042a22476bb327b5267fbb070fc79f63a5973c4c
SheilaBirgen <jeronobergen@gmail.com>
2020-03-02 16:28:11 +0300

    fixed bug in .env

--- galleria/settings.py
+++ galleria/settings.py
@@ -95,11 +95,9 @@ else:
    DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
-       )
-   }
+        )
+    }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
