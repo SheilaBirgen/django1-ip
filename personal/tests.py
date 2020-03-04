776cedbe0d2a6a022a9147ebefeac995a2809ca1
SheilaBirgen <jeronobergen@gmail.com>
2020-03-04 22:21:01 +0300

    modified tests

--- personal/tests.py
+++ personal/tests.py
@@ -4,33 +4,27 @@ from .models import Image,Location,Categories
 
 # Create your tests here.
 
-class TestLocation(TestCase):
+class LocationTestClass(TestCase):
     '''Test class to test location class'''
     def setUp(self):
         '''Function to prepare for every test case'''
-        self.test_location = Location(location = 'Eldoret')
+        self.Eldoret = Location(location = 'Eldoret')
 
     def tearDown(self):
         '''Function to clean up after every test case'''
         Location.objects.all().delete()
 
-    def test_isinstance(self):
+    def test_instance(self):
         '''Test if test_location is an instance of location class'''
-        self.assertTrue(isinstance(self.test_location, Location))
-    def test_save_location(self):
+        self.assertTrue(isinstance(self.Eldoret, Location))
+
+    def test_save_method(self):
         '''Test saving a location to database'''
-        self.test_location.save_location()
+        self.Eldoret.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
 
-    def test_delete_location(self):
-        '''Test deleting a location'''
-        self.test_location.save_class()
-        self.test_location.delete_class()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations) == 0)
-
-class TestCategories(TestCase):
+class CategoriesTestClass(TestCase):
     '''
     Test class to test categories class
     '''
@@ -42,21 +36,16 @@ class TestCategories(TestCase):
         '''Function to clean up after every test case'''
         Categories.objects.all().delete()
 
-    def test_isinstance(self):
+    def test_instance(self):
         '''
         Test if test_category is an instance of category class
         '''
-        self.assertTrue(isinstance(self.Travel,Categories))
+        self.assertTrue(isinstance(self.food,Categories))
 
     def test_save_categories(self):
         '''Test saving a category to database'''
-        self.Food.save_class()
-        categories = Category.objects.all()
+        self.food.save_categories()
+        categories = Categories.objects.all()
         self.assertTrue(len(categories) > 0)
 
-    def test_delete_categories(self):
-        '''Test deleting a category'''
-        self.test_categories.save_class()
-        self.test_categories.delete_class()
-        categories = Categories.objects.all()
-        self.assertTrue(len(categories) == 0)
+   
